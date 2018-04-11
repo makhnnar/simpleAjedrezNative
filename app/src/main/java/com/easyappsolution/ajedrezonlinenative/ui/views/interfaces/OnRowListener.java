8bb5e5bc4e59571fc566ab517b0fb72e2688f216
Pedro Gomez <pedro@jpcapps.com>
2018-04-10 23:32:59 -0400

    added nuew files

---
+++ app/src/main/java/com/easyappsolution/ajedrezonlinenative/ui/views/interfaces/OnRowListener.java
@@ -0,0 +1,11 @@
+package com.easyappsolution.ajedrezonlinenative.ui.views.interfaces;
+
+/**
+ * Created by makhnnar on 10/04/18.
+ */
+
+public interface OnRowListener {
+
+    void onRowTouch(int row);
+
+}
