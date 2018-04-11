a2a8c0e2144d69190aa668a56650a221714b8fc1
Pedro Gomez <pedro@jpcapps.com>
2018-04-11 16:57:46 -0400

    added new elements

---
+++ app/src/main/java/com/easyappsolution/ajedrezonlinenative/ui/views/interfaces/OnTableroListener.java
@@ -0,0 +1,11 @@
+package com.easyappsolution.ajedrezonlinenative.ui.views.interfaces;
+
+/**
+ * Created by makhnnar on 11/04/18.
+ */
+
+public interface OnTableroListener {
+
+    void onClickTablero(int colum,int row,boolean isOcuped);
+
+}
