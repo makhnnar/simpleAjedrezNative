776ba53b114c9f3f436e646815d1e77e38d4b5a7
Pedro Gomez <pedro@jpcapps.com>
2018-04-14 19:20:01 -0400

    update game activity

--- app/src/main/java/com/easyappsolution/ajedrezonlinenative/ui/game/view/GameActivity.kt
+++ app/src/main/java/com/easyappsolution/ajedrezonlinenative/ui/game/view/GameActivity.kt
@@ -4,12 +4,17 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.easyappsolution.ajedrezonlinenative.R
 import com.easyappsolution.ajedrezonlinenative.ui.game.mvp.GameContract.View
+import com.easyappsolution.ajedrezonlinenative.ui.game.mvp.GamePresenter
+import com.easyappsolution.ajedrezonlinenative.ui.views.interfaces.OnTableroListener
 
-class GameActivity : AppCompatActivity(), View {
+class GameActivity : AppCompatActivity(), View, OnTableroListener {
+
+    lateinit var gamePresenter : GamePresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+        gamePresenter = GamePresenter(this)
     }
 
     override fun onRemoveFicha(row:Int,colum:Int) {
@@ -18,5 +23,9 @@ class GameActivity : AppCompatActivity(), View {
 
     override fun onMoveFicha(row:Int,colum:Int) {
 
+    }
+
+    override fun onClickTablero(colum: Int, row: Int) {
+        gamePresenter.moveFicha(row,colum)
     }
 }
